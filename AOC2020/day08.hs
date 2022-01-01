d50a209dff791cebe2aa83dafe3596ec505d62be
Martin Helmer <martin@helmer.com>
2022-01-01 18:30:00 -0500

    day08

---
+++ AOC2020/day08.hs
@@ -0,0 +1,48 @@
+import qualified Data.Set as S
+import Data.List
+import qualified Data.Map as Map
+import Data.Maybe
+
+myRead :: String -> Int
+myRead (x:xs) = if x == '+' then read xs else read (x:xs)
+
+getCmdArg :: String -> (String, String)
+getCmdArg s = (take 3 s, drop 4 s)
+
+
+accUntilRepeated :: [String] -> Int -> Int -> S.Set Int -> Int
+accUntilRepeated program position acc visited  
+           | position `S.member` visited = -acc
+           | position >= length program = acc
+           | command == "nop" = accUntilRepeated program (position + 1) acc updatedSet 
+           | command == "acc" = accUntilRepeated program (position + 1) (acc + myRead arg) updatedSet 
+           | command == "jmp" = accUntilRepeated program (position + myRead arg) acc updatedSet 
+           where
+               (command,arg) = getCmdArg $ program !! position
+               updatedSet = S.insert position visited
+
+-- part 2 --
+
+variations :: [String] -> Int -> [[String]] -> [[String]]
+variations program pos l 
+          | pos >= length program = l
+          | command `elem` ["nop","jmp"] = variations program (pos+1) $ (replaceNth pos (flipNopJmp command arg) program):l
+          | otherwise  = variations program (pos+1) l
+          where 
+             (command,arg) = getCmdArg $ program !! pos
+
+flipNopJmp :: String -> String -> String
+flipNopJmp "nop" arg = concat ["jmp ",arg]
+flipNopJmp "jmp" arg = concat ["nop ",arg]
+
+
+replaceNth :: Int -> a -> [a] -> [a]
+replaceNth _ _ [] = []
+replaceNth n newVal (x:xs)
+  | n == 0 = newVal:xs
+  | otherwise = x:replaceNth (n-1) newVal xs
+
+main = do
+       l <- getContents
+       print ( accUntilRepeated (lines l) 0 0 S.empty )   -- part 1
+       print ( head $ filter (>0) $ map (\l -> accUntilRepeated (l) 0 0 S.empty ) (variations (lines l) 0 []) )
---
+++ AOC2020/day_template.hs
@@ -0,0 +1,9 @@
+import qualified Data.Set as S
+import Data.List
+import qualified Data.Map as Map
+import Data.Maybe
+
+
+main = do
+       l <- getContents
+
